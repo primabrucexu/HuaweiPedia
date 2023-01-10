1e482cc2385d06d301e1dc14b9e50b1817b2b1db
Bruce Xu <695603725@qq.com>
2023-01-10 22:36:11 +0800

    first commit

---
+++ core/src/main/java/cn/pbx/bootstrap/Main.java
@@ -0,0 +1,16 @@
+package cn.pbx.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author BruceXu
+ * @date 2023-01-10
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
